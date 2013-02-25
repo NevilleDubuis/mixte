531a1ae0cf3da5827f28d4b403a5563e44fdd3b0
Néville Dubuis <ndubuis@gmail.com>
2013-02-25 23:07:00 +0100

    banner behavior

---
+++ content/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// =require jquery
+// =require jquery-ui
+// =require jquery.h5validate
+// =require underscore
+// =require jquery-placeholder
+
+// Calcualte the home banner parallax scrolling
+  function scrollBanner() {
+    //Get the scoll position of the page
+    scrollPos = jQuery(this).scrollTop();
+
+    //Scroll and fade out the banner text
+    $('#bannertext').css({
+      'margin-top': -(scrollPos/4)+"px",
+      'opacity': 1-(scrollPos/400)
+    });
+
+    //Scroll the background of the banner
+    $('#banner').css({
+      'background-position' : 'center ' + (-scrollPos/6)+"px"
+    });
+  }
+
+jQuery(document).ready(function(){
+
+  jQuery(window).scroll(function() {
+       scrollBanner();
+  });
+
+});
